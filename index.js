c548c76f5f1d4bfd6404bd4fbc5fd920bf2cd515
Root-x12 <gmukesh058@gmail.com>
2025-09-18 21:40:29 +0530

    comment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello mukesh');
+  res.send('hello world');
 });
 
 const port = process.env.PORT || 3000;
