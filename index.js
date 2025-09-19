c111b310884bb15e37f047c8a08b8668910462a7
Root-x12 <gmukesh058@gmail.com>
2025-09-19 12:50:38 +0530

    hello world

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('test app');
+  res.send('hello wotld');
 });
 
 const port = process.env.PORT || 3000;
