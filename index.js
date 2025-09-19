bd54bd28f0f5caf9472fcaa0b2bafee5aeab0504
Root-x12 <gmukesh058@gmail.com>
2025-09-19 12:36:46 +0530

    update message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.send('test app');
 });
 
 const port = process.env.PORT || 3000;
